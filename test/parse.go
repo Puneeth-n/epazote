ddfc388004da0c9c73c6e7252cebac8cec07ff71
Nicolas Embriz <nbari@Nicolass-iMac.local>
2015-12-13 22:05:40 +0100

    modified:   config.yaml modified:   parse.go

--- test/parse.go
+++ test/parse.go
@@ -9,12 +9,12 @@ import (
 type Services map[string]Service
 
 type Service struct {
-	URL                     string
-	Timeout                 int
-	Seconds, Minutes, Hours int
-	Expect                  Expect
-	Log                     string
-	If_header, If_status    map[string]Action
+	URL                           string
+	Timeout                       int
+	Seconds, Minutes, Hours       int
+	Log                           string
+	If_not                        Expect
+	If_status, If_header, If_body map[string]Action
 }
 
 type Action struct {
@@ -26,6 +26,7 @@ type Action struct {
 type Expect struct {
 	Status int
 	Header map[string]string
+	Body   string
 	Action Action
 }
 
@@ -42,5 +43,10 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(data["service 1"], "\n", data["service 1"].Seconds, data)
+	fmt.Println(data["service 1"].If_not.Body)
+
+	for k, v := range data["service 1"].If_not.Header {
+		fmt.Println(k, v)
+	}
+
 }
