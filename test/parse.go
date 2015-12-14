ebf31cc963ac2458372eaa1279f3b1b609d2c963
nbari <nbari@dalmp.com>
2015-12-14 13:12:27 +0100

    modified:   config.yaml 	modified:   parse.go

--- test/parse.go
+++ test/parse.go
@@ -2,32 +2,50 @@ package main
 
 import (
 	"fmt"
+	"github.com/kr/pretty"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 )
 
-type Services map[string]Service
-
-type Service struct {
-	URL                           string
-	Timeout                       int
-	Seconds, Minutes, Hours       int
-	Log                           string
-	If_not                        Expect
-	If_status, If_header, If_body map[string]Action
+type Epazote struct {
+	Config   Config
+	Services map[string]Service
 }
 
-type Action struct {
-	Cmd    string
-	Notify string
-	Msg    string
+type Config struct {
+	SMTP Email `yaml:"smtp"`
+}
+
+type Email struct {
+	Username string
+	Password string
+	Host     string
+	Port     int
+	Tls      bool
+	Headers  map[string]string
+}
+
+type Service struct {
+	URL                     string
+	Timeout                 int
+	Seconds, Minutes, Hours int
+	Log                     string
+	Expect                  Expect
+	IfStatus                map[string]Action `yaml:"if_status`
+	IfHeader                map[string]Action `yaml:"if_header"`
 }
 
 type Expect struct {
 	Status int
 	Header map[string]string
 	Body   string
-	Action Action
+	IfNot  Action `yaml:"if_not"`
+}
+
+type Action struct {
+	Cmd    string
+	Notify string
+	Msg    string
 }
 
 func main() {
@@ -37,16 +55,17 @@ func main() {
 		panic(err)
 	}
 
-	var data Services
+	var data Epazote
 
 	if err := yaml.Unmarshal(yml_file, &data); err != nil {
 		panic(err)
 	}
 
-	fmt.Println(data["service 1"].If_not.Body)
+	fmt.Printf("%# v", pretty.Formatter(data.Config.SMTP.Username))
 
-	for k, v := range data["service 1"].If_not.Header {
-		fmt.Println(k, v)
-	}
+	//	fmt.Println(data["service 1"].Expect.IfNot)
 
+	//for k, v := range data["service 1"].Expect.Header {
+	//	fmt.Println(k, v)
+	//}
 }
