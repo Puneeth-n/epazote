5a4dfbf14a32d31e21336b2581eaadf9364de526
nbari <nbari@dalmp.com>
2015-12-15 21:36:18 +0100

    new file:   .gitignore 	new file:   mail.go 	modified:   main.go

---
+++ mail.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"github.com/nbari/epazote/config"
+	"log"
+	"net/smtp"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const CRLF = "\r\n"
+
+func SendEmail(e config.Email) {
+	// auth Set up authentication information.
+	auth := smtp.PlainAuth("",
+		e.Username,
+		e.Password,
+		e.Host,
+	)
+
+	// set From
+	if _, ok := e.Headers["from"]; !ok {
+		name, err := os.Hostname()
+		if err != nil {
+			log.Println(err)
+		}
+		e.Headers["from"] = "epazote@" + name
+	}
+
+	// set To
+	to := strings.Split(e.Headers["to"], " ")
+
+	// add headers
+	e.Headers["MIME-Version"] = "1.0"
+	e.Headers["Content-Type"] = "text/plain; charset=UTF-8"
+	e.Headers["Content-Transfer-Encoding"] = "base64"
+
+	// email Body
+	body := ""
+
+	// message template
+	msg := ""
+	for k, v := range e.Headers {
+		if k == "to" {
+			msg += fmt.Sprintf("%s: %s %s", strings.Title(k), strings.Join(to, ", "), CRLF)
+		} else {
+			msg += fmt.Sprintf("%s: %s %s", strings.Title(k), strings.TrimSpace(v), CRLF)
+		}
+	}
+	msg += CRLF + base64.StdEncoding.EncodeToString([]byte(body))
+
+	err := smtp.SendMail(
+		e.Host+":"+strconv.Itoa(e.Port),
+		auth,
+		e.Headers["from"],
+		to,
+		[]byte(msg),
+	)
+
+	if err != nil {
+		log.Println("ERROR: attempting to send a mail ", err)
+	}
+
+}
--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func main() {
 	flag.Parse()
 
 	if _, err := os.Stat(*f); os.IsNotExist(err) {
-		fmt.Printf("Can't read file: %s, use -h for more info.\n\n", *f)
+		fmt.Printf("Cannot read file: %s, use -h for more info.\n\n", *f)
 		os.Exit(1)
 	}
 
@@ -26,11 +26,12 @@ func main() {
 		panic(err)
 	}
 
-	//	fmt.Printf("%#v", epazote.Config.SMTP.Username)
-
 	if *v {
 		fmt.Printf("%# v", pretty.Formatter(epazote))
-		fmt.Println(epazote.Config.SMTP)
 	}
+
+	fmt.Printf("%# v", epazote.Config.SMTP)
+
+	// 	SendEmail(epazote.Config.SMTP)
 
 }
