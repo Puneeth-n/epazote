9caec496c3a95a51738bcec7896fb810f823c836
nbari <nbari@dalmp.com>
2015-12-31 20:45:50 +0100

    modified:   supervice.go

--- supervice.go
+++ supervice.go
@@ -8,6 +8,7 @@ import (
 // Supervice check services
 func Supervice(s Service) func() {
 	return func() {
+		// to determine if body should be fetched or not
 		get_body := false
 		switch t := s.Expect.Body.(type) {
 		case nil:
@@ -17,7 +18,12 @@ func Supervice(s Service) func() {
 			get_body = true
 		}
 
-		log.Println(get_body)
+		res, err := Get(s.URL, s.Timeout)
+		if err != nil {
+			log.Println(err)
+		}
+
+		log.Println(get_body, res)
 
 	}
 }
