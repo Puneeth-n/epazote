01e6fc92ecab35064d60e9c502f75caa666dd540
nbari <nbari@dalmp.com>
2016-01-13 20:48:17 +0100

    modified:   request_test.go

--- request_test.go
+++ request_test.go
@@ -1,12 +1,58 @@
 package epazote
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestHTTPGet(t *testing.T) {
-	_, err := HTTPGet("http://google.com", 3)
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("User-agent") != "epazote" {
+			t.Error("Expecting User-agent: epazote")
+		}
+		fmt.Fprintln(w, "Hello, epazote")
+	}))
+	defer ts.Close()
+
+	res, err := HTTPGet(ts.URL, 3)
 	if err != nil {
 		t.Error(err)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(body) != "Hello, epazote\n" {
+		t.Error("Expecting Hello, epazote")
+	}
+
+	if res.StatusCode != 200 {
+		t.Error("Expecting StatusCode 200")
+	}
+}
+
+func TestAsyngGet(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("User-agent") != "epazote" {
+			t.Error("Expecting User-agent: epazote")
+		}
+		fmt.Fprintln(w, "Hello, epazote")
+	}))
+	defer ts.Close()
+	s := make(Services)
+	s["s 1"] = Service{
+		URL: ts.URL,
+	}
+	ch := AsyncGet(s)
+	for i := 0; i < len(s); i++ {
+		x := <-ch
+		if x.Err != nil {
+			t.Error(x.Err)
+		}
 	}
 }
