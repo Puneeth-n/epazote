99de55e9c05b51b3f94529ca70efc550ce45cd2d
nbari <nbari@dalmp.com>
2016-01-18 17:18:22 +0100

    modified:   mail_test.go 	modified:   test/epazote-email.yml

--- mail_test.go
+++ mail_test.go
@@ -1,6 +1,7 @@
 package epazote
 
 import (
+	"gopkg.in/yaml.v2"
 	"testing"
 )
 
@@ -24,4 +25,37 @@ func TestVerifyEmail(t *testing.T) {
 	if err != nil {
 		t.Errorf("Expecting error: %s", err)
 	}
+}
+
+func TestVerifyEmail2(t *testing.T) {
+	var conf = `
+config:
+    smtp:
+        username: username
+        password: password
+        server: smtp.server
+        port: 587
+        headers:
+            from: from@email
+            to: team@email
+            subject: >
+                [%s - %s], Service, Status
+    services:
+        service 1:
+            expect:
+                if_not:
+                    notify: yes
+                if_status:
+                    502:
+                        notify: yes
+                if_header:
+                    x-db-kaputt:
+                        notify: yes
+`
+	var ez Epazote
+
+	if err := yaml.Unmarshal([]byte(conf), &ez); err != nil {
+		t.Error(err)
+	}
+
 }
