e3e1392de952a1abf793cd9ff7cf262dba254e92
nbari <nbari@dalmp.com>
2016-01-20 14:39:12 +0100

    modified:   supervice_test.go

--- supervice_test.go
+++ supervice_test.go
@@ -263,7 +263,7 @@ func TestSuperviceNoGet(t *testing.T) {
 		}
 		// check output
 		if o, ok := i["output"]; ok {
-			e := "No defined cmd"
+			e := "exit status 1"
 			if o != e {
 				t.Errorf("Expecting %q, got %q", e, o)
 			}
@@ -284,6 +284,76 @@ func TestSuperviceNoGet(t *testing.T) {
 		Log:  log_s.URL,
 		Expect: Expect{
 			Status: 200,
+			IfNot: Action{
+				Cmd: "test 1 -gt 2",
+			},
+		},
+	}
+	ez := &Epazote{
+		Services: s,
+	}
+	wg.Add(1)
+	ez.Supervice(s["s 1"])()
+	wg.Wait()
+}
+func TestSuperviceNoGetStatus0(t *testing.T) {
+	var wg sync.WaitGroup
+	log_s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("User-agent") != "epazote" {
+			t.Error("Expecting User-agent: epazote")
+		}
+		decoder := json.NewDecoder(r.Body)
+		var i map[string]interface{}
+		err := decoder.Decode(&i)
+		if err != nil {
+			t.Error(err)
+		}
+		// check name
+		if n, ok := i["name"]; ok {
+			if n != "s 1" {
+				t.Errorf("Expecting  %q, got: %q", "s 1", n)
+			}
+		} else {
+			t.Errorf("key not found: %q", "name")
+		}
+		// check because
+		if b, ok := i["because"]; ok {
+			e := "GET: Get http://: http: no Host in request URL"
+			if b != e {
+				t.Errorf("Expecting: %q, got: %q", e, b)
+			}
+		} else {
+			t.Errorf("key not found: %q", "because")
+		}
+		// check exit
+		if e, ok := i["exit"]; ok {
+			if e.(float64) != 1 {
+				t.Errorf("Expecting: 1 got: %v", e.(float64))
+			}
+		} else {
+			t.Errorf("key not found: %q", "exit")
+		}
+		// check output
+		if o, ok := i["output"]; ok {
+			t.Errorf("key should not exist,content: %q", o)
+		}
+		// check url
+		if _, ok := i["url"]; !ok {
+			t.Error("URL key not found")
+		}
+		wg.Done()
+	}))
+	defer log_s.Close()
+	s := make(Services)
+	s["s 1"] = Service{
+		Name: "s 1",
+		URL:  "http://",
+		Log:  log_s.URL,
+		Expect: Expect{
+			Status: 200,
+			IfNot: Action{
+				Cmd: "test 3 -gt 2",
+			},
 		},
 	}
 	ez := &Epazote{
