6f74fb650405556a73ffd12fb40b3ddacf5debbb
nbari <nbari@dalmp.com>
2016-01-27 10:51:38 +0100

    modified:   supervice_test.go

--- supervice_test.go
+++ supervice_test.go
@@ -1243,3 +1243,171 @@ func TestSuperviceMatchingHeaderDebugRed(t *testing.T) {
 		t.Error("Expecting log.Println error")
 	}
 }
+
+func TestSupervice302(t *testing.T) {
+	buf.Reset()
+	var wg sync.WaitGroup
+	check_s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("User-agent") != "epazote" {
+			t.Error("Expecting User-agent: epazote")
+		}
+		w.WriteHeader(http.StatusFound)
+	}))
+	defer check_s.Close()
+	log_s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("User-agent") != "epazote" {
+			t.Error("Expecting User-agent: epazote")
+		}
+		decoder := json.NewDecoder(r.Body)
+		var i map[string]interface{}
+		err := decoder.Decode(&i)
+		if err != nil {
+			t.Error(err)
+		}
+		// check name
+		if n, ok := i["name"]; ok {
+			if n != "s 1" {
+				t.Errorf("Expecting  %q, got: %q", "s 1", n)
+			}
+		} else {
+			t.Errorf("key not found: %q", "name")
+		}
+		// check because
+		if b, ok := i["because"]; ok {
+			if b != "Status: 302" {
+				t.Errorf("Expecting: %q, got: %q", "Status: 302", b)
+			}
+		} else {
+			t.Errorf("key not found: %q", "because")
+		}
+		// check exit
+		if e, ok := i["exit"]; ok {
+			if e.(float64) != 0 {
+				t.Errorf("Expecting: 1 got: %v", e.(float64))
+			}
+		} else {
+			t.Errorf("key not found: %q", "exit")
+		}
+		// check url
+		if _, ok := i["url"]; !ok {
+			t.Error("URL key not found")
+		}
+		// check output
+		if o, ok := i["output"]; ok {
+			t.Errorf("key should not exist,content: %q", o)
+		}
+		if i["status"].(float64) != 302 {
+			t.Errorf("Expecting status: %d got: %v", 302, i["status"])
+		}
+		wg.Done()
+	}))
+	defer log_s.Close()
+	s := make(Services)
+	s["s 1"] = Service{
+		Name: "s 1",
+		URL:  check_s.URL,
+		Log:  log_s.URL,
+		Expect: Expect{
+			Status: 302,
+			IfNot:  Action{},
+		},
+		IfStatus: map[int]Action{
+			200: Action{},
+		},
+	}
+	ez := &Epazote{
+		Services: s,
+		debug:    true,
+	}
+	wg.Add(1)
+	ez.Supervice(s["s 1"])()
+	wg.Wait()
+
+	if buf.Len() == 0 {
+		t.Error("Expecting log.Println error")
+	}
+}
+
+func TestSuperviceFollow(t *testing.T) {
+	buf.Reset()
+	var wg sync.WaitGroup
+	check_end := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, epazote match 0BC20225-2E72-4646-9202-8467972199E1 regex")
+	}))
+	defer check_end.Close()
+	check_s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("User-agent") != "epazote" {
+			t.Error("Expecting User-agent: epazote")
+		}
+		http.Redirect(w, r, check_end.URL, http.StatusFound)
+	}))
+	defer check_s.Close()
+	log_s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("User-agent") != "epazote" {
+			t.Error("Expecting User-agent: epazote")
+		}
+		decoder := json.NewDecoder(r.Body)
+		var i map[string]interface{}
+		err := decoder.Decode(&i)
+		if err != nil {
+			t.Error(err)
+		}
+		// check name
+		if n, ok := i["name"]; ok {
+			if n != "s 1" {
+				t.Errorf("Expecting  %q, got: %q", "s 1", n)
+			}
+		} else {
+			t.Errorf("key not found: %q", "name")
+		}
+		// check because
+		e := "Body regex match: 0BC20225-2E72-4646-9202-8467972199E1"
+		if i["because"] != e {
+			t.Errorf("Expecting: %q, got: %v", e, i["because"])
+		}
+		// check exit
+		if i["exit"].(float64) != 0 {
+			t.Errorf("Expecting: 0 got: %v", i["exit"])
+		}
+		// check url
+		if _, ok := i["url"]; !ok {
+			t.Error("URL key not found")
+		}
+		// check output
+		if o, ok := i["output"]; ok {
+			t.Errorf("key should not exist,content: %q", o)
+		}
+		if i["status"].(float64) != 200 {
+			t.Errorf("Expecting status: %d got: %v", 200, i["status"])
+		}
+		wg.Done()
+	}))
+	defer log_s.Close()
+	s := make(Services)
+	re := regexp.MustCompile(`(?i)[a-z0-9]{8}-[a-z0-9]{4}-[1-5][a-z0-9]{3}-[a-z0-9]{4}-[a-z0-9]{12}`)
+	s["s 1"] = Service{
+		Name:   "s 1",
+		URL:    check_s.URL,
+		Follow: true,
+		Log:    log_s.URL,
+		Expect: Expect{
+			Status: 200,
+			Body:   "(?i)[a-z0-9]{8}-[a-z0-9]{4}-[1-5][a-z0-9]{3}-[a-z0-9]{4}-[a-z0-9]{12}",
+			body:   re,
+		},
+		IfStatus: map[int]Action{
+			302: Action{},
+		},
+	}
+	ez := &Epazote{
+		Services: s,
+		debug:    true,
+	}
+	wg.Add(1)
+	ez.Supervice(s["s 1"])()
+	wg.Wait()
+
+	if buf.Len() == 0 {
+		t.Error("Expecting log.Println error")
+	}
+}
