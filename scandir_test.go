d51cf237f9ac7c65f77606ae4af3f11913ba3e16
nbari <nbari@dalmp.com>
2016-02-18 20:25:14 +0100

    modified:   scandir_test.go

--- scandir_test.go
+++ scandir_test.go
@@ -129,3 +129,66 @@ func TestScanParseScanSearchBadRegex(t *testing.T) {
 
 	buf.Reset()
 }
+
+func TestScanParseScanLast(t *testing.T) {
+	buf.Reset()
+	dir := "./"
+	prefix := "test-scan2-"
+
+	d, err := ioutil.TempDir(dir, prefix)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer os.RemoveAll(d)
+
+	f := []byte(`
+    service 1:
+        url: http://about.epazote.io
+        expect:
+            status: 402
+`)
+
+	err = ioutil.WriteFile(fmt.Sprintf("%s/epazote.yml", d), f, 0644)
+	s := make(Services)
+	s["service 1"] = &Service{
+		Name: "service 1",
+		Expect: Expect{
+			Status: 200,
+			IfNot: Action{
+				Notify: "yes",
+			},
+		},
+		status: 3,
+		action: &Action{Cmd: "matilde"},
+	}
+
+	ez := &Epazote{
+		Services: s,
+	}
+	ez.search(d)
+	if err != nil {
+		t.Error(err)
+	}
+
+	sk := GetScheduler()
+
+	if len(sk.Schedulers) != 1 {
+		t.Error("Expecting 1")
+	}
+
+	if s["service 1"].Expect.Status != 402 {
+		t.Errorf("Expecting status 402 got: %v", s["service 1"].Expect.Status)
+	}
+
+	if s["service 1"].status != 3 {
+		t.Errorf("Expecting status 3 got: %v", s["service 1"].status)
+	}
+
+	if s["service 1"].action.Cmd != "matilde" {
+		t.Errorf("Expecting Cmd = matilde got: %v", s["service 1"].action.Cmd)
+	}
+
+	buf.Reset()
+}
